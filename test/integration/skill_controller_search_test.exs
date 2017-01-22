066900ba60074da344d2ac8cab877951f8711c8f
Paul Sullivan <paulsullivan1@gmail.com>
2017-01-21 23:28:03 -0500

    Moved some code and a couple small refactors of the helper module

--- test/integration/skill_controller_search_test.exs
+++ test/integration/skill_controller_search_test.exs
@@ -1,29 +1,50 @@
 defmodule SkillControllerSearchIntegrationTest do
   use ExUnit.Case, async: true
   alias CodeCorps.ElasticSearchHelper
+  alias Elastix.Document
 
   @test_url Application.get_env(:code_corps, :elasticsearch_url)
   @test_index  Application.get_env(:code_corps, :elasticsearch_index)
+  @type_value "title"
 
   setup do
-    Elastix.Index.delete(Application.get_env(:code_corps, :elasticsearch_url),
-      Application.get_env(:code_corps, :elasticsearch_index))
+    ElasticSearchHelper.delete(@test_url, @test_index)
+    ElasticSearchHelper.create_index(@test_url, @test_index, @type_value)
+    init()
     :ok
   end
 
   test "search partial word" do
-   results = ElasticSearchHelper.search("ru")
+   results = ElasticSearchHelper.search(@test_url, @test_index, "ru")
    assert results == ["Ruby"]
   end
 
   test "search whole word" do
-    results = ElasticSearchHelper.search("css")
+    results = ElasticSearchHelper.search(@test_url, @test_index, "css")
     assert results == ["CSS"]
   end
 
   test "search no matches" do
-    results = ElasticSearchHelper.search("foo")
+    results = ElasticSearchHelper.search(@test_url, @test_index, "foo")
     assert results == []
   end
 
+  def init do
+    data = %{
+      title: "Elixir"
+    }
+    Document.index @test_url, @test_index, @type_value, 1, data, [refresh: true]
+    data = %{
+      title: "Ruby"
+    }
+    Document.index @test_url, @test_index, @type_value, 2, data, [refresh: true]
+    data = %{
+      title: "Rails"
+    }
+    Document.index @test_url, @test_index, @type_value, 3, data, [refresh: true]
+    data = %{
+      title: "CSS"
+    }
+    Document.index @test_url, @test_index, @type_value, 4, data, [refresh: true]
+  end
 end
--- web/helpers/elastic_search_helper.ex
+++ web/helpers/elastic_search_helper.ex
@@ -1,7 +1,6 @@
 defmodule CodeCorps.ElasticSearchHelper do
   alias Elastix.Search
   alias Elastix.Index
-  alias Elastix.Document
 
   @test_url Application.get_env(:code_corps, :elasticsearch_url)
   @test_index  Application.get_env(:code_corps, :elasticsearch_index)
@@ -15,39 +14,14 @@ defmodule CodeCorps.ElasticSearchHelper do
     Index.settings(url, "#{index}/_mapping/#{type}", field_filter)
   end
 
-  def init do
-    Index.settings(@test_url, @test_index, settings_map)
-    Index.settings(@test_url, "skills/_mapping/title", field_filter)
-
-    data = %{
-      title: "CSS"
-    }
-    Document.index @test_url, @test_index, "title", 1, data, [refresh: true]
-    data = %{
-      title: "Elixir"
-    }
-    Document.index @test_url, @test_index, "title", 2, data, [refresh: true]
-    data = %{
-      title: "Ruby"
-    }
-    Document.index @test_url, @test_index, "title", 3, data, [refresh: true]
-
-    data = %{
-      title: "Rails"
-    }
-    Document.index @test_url, @test_index, "title", 4, data, [refresh: true]
-  end
-
-  def search(search_query) do
-    init
-
+  def search(url, index, search_query) do
     data = %{
       query: %{
         match: %{ title: search_query }
       }
     }
 
-    response = Search.search @test_url, @test_index, [], data
+    response = Search.search url, index, [], data
 
     #response.status_code == 200 do
     #count = response.body["hits"]["total"]
