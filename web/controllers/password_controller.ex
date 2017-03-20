ecd8f0573cef4cd412f1a74af2b4bfc57a7adee4
snewcomer <snewcomer24@gmail.com>
2017-03-20 16:11:30 -0700

    forgot password service

--- web/controllers/password_controller.ex
+++ web/controllers/password_controller.ex
@@ -1,20 +1,13 @@
 defmodule CodeCorps.PasswordController do
   use CodeCorps.Web, :controller
 
-  alias CodeCorps.{Emails, Mailer, User, AuthToken}
+  alias CodeCorps.{Services.ForgotPasswordService}
 
   @doc"""
   forgot_password should take an email and generate an AuthToken model and send an email
   """
   def forgot_password(conn, %{"email" => email}) do
-    with %User{} = user <- Repo.get_by(User, email: email),
-        { :ok, %AuthToken{} = %{ value: token } } <- AuthToken.changeset(%AuthToken{}, user) |> Repo.insert
-    do
-      Emails.ForgotPasswordEmail.create(user, token) |> Mailer.deliver_now()
-    else
-      nil -> nil
-    end
-
+    ForgotPasswordService.forgot_password(email) 
     conn |> put_status(:ok) |> render("show.json", email: email)
   end
 
